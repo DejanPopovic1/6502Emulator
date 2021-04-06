6eb2fa19d2c9851f534835d0b47f1e7c376eeeb9
Dejan Popovic <dejan.dp.popovic@gmail.com>
2021-04-06 20:58:57 +0200

    Core CPU data structure

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <string>
 
+struct CPU{
+    using Byte = unsigned char;
+    using Word = unsigned short;
+
+    Word PC;
+    Word SP;
+
+    Byte A, X, Y;
+
+    Byte C : 1;
+    Byte Z : 1;
+    Byte I : 1;
+    Byte D : 1;
+    Byte B : 1;
+    Byte V : 1;
+    Byte N : 1;
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::string test;
