ac8a6438e0908b7f5ac66511580e0d4f8cd52310
Dejan Popovic <dejan.dp.popovic@gmail.com>
2021-04-06 21:32:44 +0200

    memory reset

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <string>
 
+using Byte = unsigned char;
+using Word = unsigned short;
+using u32 = unsigned int;
+
+struct Mem{
+    static constexpr u32 MAX_MEM = 1024 * 64;
+    Byte Data[MAX_MEM];
+
+    void Initialise(){
+        for(u32 i = 0; i < MAX_MEM; i++) {
+            Data[i] = 0;
+        }
+    }
+};
+
 struct CPU{
-    using Byte = unsigned char;
-    using Word = unsigned short;
+
+
+
     Word PC;
     Word SP;
     Byte A, X, Y;
@@ -15,18 +31,20 @@ struct CPU{
     Byte V : 1;
     Byte N : 1;
 
-    void Reset(){
+    void Reset(Mem & memory){
         PC = 0xFFFC;
         SP = 0x0100;
         C = Z = I = D = B = V = N = 0;
         A = X = Y = 0;
+        memory.Initialise();
     }
 
 };
 
 int main() {
+    Mem mem;
     CPU cpu;
-    cpu.Reset();
+    cpu.Reset(mem);
     std::string test;
     std::cin >> test;
     return 0;
