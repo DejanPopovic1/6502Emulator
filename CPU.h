4b212079cbbf7aca5feae4146b1cbc1b3e06620c
Dejan Popovic <dejan.dp.popovic@gmail.com>
2021-04-09 23:34:32 +0200

    primed CPU and Mem files

---
+++ CPU.h
@@ -0,0 +1,17 @@
+//
+// Created by DejanP on 2021/04/09.
+//
+
+#ifndef INC_6502EMULATOR_CPU_H
+#define INC_6502EMULATOR_CPU_H
+
+class CPU{
+
+
+
+
+
+};
+
+
+#endif //INC_6502EMULATOR_CPU_H
---
+++ Mem.h
@@ -0,0 +1,15 @@
+//
+// Created by DejanP on 2021/04/09.
+//
+
+#ifndef INC_6502EMULATOR_MEM_H
+#define INC_6502EMULATOR_MEM_H
+
+class Mem{
+
+
+
+
+};
+
+#endif //INC_6502EMULATOR_MEM_H
