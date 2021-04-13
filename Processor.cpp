b706c57983892eedba590a0b94b6c8394afbb972
Dejan Popovic <dejan.dp.popovic@gmail.com>
2021-04-13 13:16:23 +0200

    implemented BPL()

--- Processor.cpp
+++ Processor.cpp
@@ -338,7 +338,16 @@ uint8_t Processor::BNE()
 
 uint8_t Processor::BPL()
 {
+    if (getFlag(N) == 0)
+    {
+        cycles++;
+        addr_abs = PC + addr_rel;
 
+        if ((addr_abs & 0xFF00) != (PC & 0xFF00))
+            cycles++;
+
+        PC = addr_abs;
+    }
     return 0;
 }
 
