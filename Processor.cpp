c18150ae5803b79787dfc734daf2a8890109e6e7
Dejan Popovic <dejan.dp.popovic@gmail.com>
2021-04-15 12:38:46 +0200

    adjusted BIT()

--- Processor.cpp
+++ Processor.cpp
@@ -399,8 +399,8 @@ uint8_t Processor::BEQ(){
 uint8_t Processor::BIT(){
     fetch();
     setOrClearFlag(Z, !fetched & A);
-    setOrClearFlag(V, fetched & 0x40);//Sixth bit. Add macro's
-    setOrClearFlag(N, fetched & 0x80);//Seventh bit. Add macro's
+    setOrClearFlag(V, fetched & 0x40);//Sixth bit. Add macro's. Rather use 1 << 6 as the macro
+    setOrClearFlag(N, fetched & 0x80);//Seventh bit. Add macro's. Rather use 1 << 7 as the macro
     return 0;
 }
 
